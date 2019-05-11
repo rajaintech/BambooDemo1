5d74056607a32413411e3df2976068a129997292
Raja Bhoopalan <rajaintech@gmail.com>
2019-05-11 22:11:02 +1000

    adding source

---
+++ src/main/java/com/example/BambooDemo1/BambooController.java
@@ -0,0 +1,15 @@
+package com.example.BambooDemo1;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BambooController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello Bamboo...";
+    }
+
+}
